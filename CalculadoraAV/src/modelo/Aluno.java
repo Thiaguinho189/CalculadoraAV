e1a16acfa5d3114d2b46e1846592f9383c93735f
94421340278 <94421340278@JIPA-LABIN02-28.ifro.local>
2018-10-10 10:51:47 -0400

    fjlkdsjlkfjdfslkj

--- CalculadoraAV/src/modelo/Aluno.java
+++ CalculadoraAV/src/modelo/Aluno.java
@@ -1,9 +1,59 @@
 package modelo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 
 @Entity
 public class Aluno {
-    private id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Double valor1;
+    private Double valor2;
+    private String operador;
+    private Double resultado;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getValor1() {
+        return valor1;
+    }
+
+    public void setValor1(Double valor1) {
+        this.valor1 = valor1;
+    }
+
+    public Double getValor2() {
+        return valor2;
+    }
+
+    public void setValor2(Double valor2) {
+        this.valor2 = valor2;
+    }
+
+    public String getOperador() {
+        return operador;
+    }
+
+    public void setOperador(String operador) {
+        this.operador = operador;
+    }
+
+    public Double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Double resultado) {
+        this.resultado = resultado;
+    }
+    
 }
