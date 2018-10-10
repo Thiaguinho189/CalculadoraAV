e3e6c0999c99402f5ad5f8c6486708ac59b09543
94421340278 <94421340278@JIPA-LABIN02-28.ifro.local>
2018-10-10 11:00:45 -0400

    fjlkdsjlkfjdfslkj

--- CalculadoraAV/src/control/CalculadoraController.java
+++ CalculadoraAV/src/control/CalculadoraController.java
@@ -7,7 +7,10 @@ package control;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,12 +19,49 @@ import javafx.fxml.Initializable;
  */
 public class CalculadoraController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button btn7;
+    @FXML
+    private Button btn4;
+    @FXML
+    private Button btn8;
+    @FXML
+    private Button btn5;
+    @FXML
+    private Button btn9;
+    @FXML
+    private Button btn6;
+    @FXML
+    private Button btn1;
+    @FXML
+    private Button btn2;
+    @FXML
+    private Button btn3;
+    @FXML
+    private Button btn0;
+    @FXML
+    private Button btnIgual;
+    @FXML
+    private Button btnDiv;
+    @FXML
+    private Button btnMenos;
+    @FXML
+    private Button btnMais;
+    @FXML
+    private Button btnMulti;
+    @FXML
+    private Button btnCE;
+    @FXML
+    private TextField btnResult;
+
+    @FXML
+    public void
+            
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    
+       
+      
     }    
     
 }
