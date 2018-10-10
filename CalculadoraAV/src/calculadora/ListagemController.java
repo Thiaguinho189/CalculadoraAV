17861bd1a88f634ebb7c15833e28c5973a722427
94421340278 <94421340278@JIPA-LABIN02-28.ifro.local>
2018-10-10 16:03:11 -0400

    fjlkdsjlkfjdfslkj

---
+++ CalculadoraAV/src/calculadora/ListagemController.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ * FXML Controller class
+ *
+ * @author 02977022201
+ */
+public class ListagemController implements Initializable {
+
+    @FXML
+    private TableView<?> tabela;
+   
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Calculadora");
+        EntityManager em = emf.createEntityManager();
+
+        // Busca utilizando HQL
+        Query query = em.createQuery("SELECT a FROM Calcles as a");
+        List<Calcles> alunos = query.getResultList();
+
+        // Converte lista para observable list
+        ObservableList ob = FXCollections.observableArrayList(alunos);
+        // Adiciona resultado a Tabela
+        tabela.setItems(ob);
+        // Adiciona resultado ao Combobox
+               
+
+        em.close();
+        emf.close();
+    }
+
+    
+}
---
+++ CalculadoraAV/src/calculadora/LoginController.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ * FXML Controller class
+ *
+ * @author 04154521209
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private JFXTextField txtusu;
+    @FXML
+    private JFXPasswordField txtsenha;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void cadastrarL(ActionEvent event) throws IOException {
+  FXMLLoader fxmlLoader = new FXMLLoader();
+    fxmlLoader.setLocation(getClass().getResource("cadastro.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(),500,500);
+    Stage stage = new Stage();
+    stage.setTitle("Cadastro");
+    stage.setScene(scene);
+    stage.show();
+    }
+    
+
+    @FXML
+    private void entrar(ActionEvent event)throws IOException  {
+      EntityManagerFactory emf = Persistence.createEntityManagerFactory("Calculadora");
+      EntityManager em = emf.createEntityManager();
+        
+        Query query = em.createQuery("SELECT f FROM Login as f WHERE f.usuario = :user ");
+        query.setParameter("user", txtusu.getText());
+        if(query.getResultList().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setHeaderText("o usuario nao existe BROW");
+            alert.showAndWait();
+        }else{
+            Login f =(Login) query.getSingleResult();
+            if (!f.getSenha().equals(txtsenha.getText())){
+              Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setHeaderText("A senha nao existe BROW");
+            alert.showAndWait();
+            }
+            else{
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("MenuPrincipal.fxml"));
+                Scene scene = new Scene(fxmlLoader.load(),500,500);
+                Stage stage = new Stage();
+                stage.setTitle("Menu Principal");
+                stage.setScene(scene);
+                stage.show();
+        }
+                 
+                
+
+    }
+   
+    }
+}
+
+      
